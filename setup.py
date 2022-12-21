84a8bc9449ff64577e46c40243b8bf80a227729e
Zackary <zspink@nea.k12.ar.us>
2022-12-20 20:32:46 -0600

    modification

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='aels_license_validator',
     version="1.0",
-    packages=find_packages(),
+    packages=["aels_license_validator"],
     install_requires=[
         "playwright",
         "bs4"
